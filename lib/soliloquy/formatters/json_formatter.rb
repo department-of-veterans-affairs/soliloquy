f9672421f78d3988511fc19a2cfeea47f32978b2
Alastair Dawson <alastair.j.dawson@gmail.com>
2016-12-14 15:10:13 -0800

    don't use abbreviation for datetime, severity in JSON formatter (#4)

--- lib/soliloquy/formatters/json_formatter.rb
+++ lib/soliloquy/formatters/json_formatter.rb
@@ -5,15 +5,14 @@ module Soliloquy
   module Formatters
     class JSON
       def self.format(highlight = false)
-        highlighter = Soliloquy::JSONHighlighter if highlight
         proc do |severity, datetime, _progname, msg|
           h = {
-            t: datetime.utc.strftime('%Y-%m-%d %H:%M:%S'),
-            s: severity
+            datetime: datetime.utc.strftime('%Y-%m-%d %H:%M:%S'),
+            severity: severity
           }
           h.merge! msg if msg.is_a? Hash
           message = "#{Oj.dump(h, mode: :compat)}\n"
-          message = highlighter.highlight(message, severity) if highlighter
+          message = Soliloquy::JSONHighlighter.highlight(message, severity) if highlight
           message
         end
       end
